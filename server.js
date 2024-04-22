ff6f3f2300c4f95533357b3bd4e15bdac1a005a6
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-04-22 08:25:35 -0500

    feat(project): server, use connect history api fallback to support router in vue

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import history from 'connect-history-api-fallback'
 
 const port = 8080
 const app = express()
 
+app.use(history())
 app.use(express.static('dist'))
 
 app.listen(port, () => console.log(`Server running on port ${port}: http://localhost:${port}`))
